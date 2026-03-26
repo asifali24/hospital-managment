c3ef9b8045aea025ee57910fef7a7f0232ff1200
asifali24 <aliasifali231@gmail.com>
2026-03-26 22:08:50 +0530

    initial commit

---
+++ src/test/java/com/RESTAPI_2/hospitalManagement/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RESTAPI_2.hospitalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
